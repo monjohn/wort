b7400133b4bf1a5820b830e4de7bcde8572ec349
Monte Johnston <johnston.monte@gmail.com>
2017-01-14 10:44:15 -0500

    Displays word pairs

--- src/main.rs
+++ src/main.rs
@@ -13,36 +13,48 @@ use std::io::BufRead;
 
 
 fn main() {
-    let word = match env::args().nth(1) {
-        None =>  "alabasterpolyaster".to_string(),
-        Some(ref w) => w.to_string()
-    };
+    let word = env::args().nth(1).expect("You must enter a word.");
 
-   // a.unwrap_or("bar".to_string())
+    let word_re = "^".to_string() + &word + " ";
+    // a.unwrap_or("bar".to_string())
 
     let start = PreciseTime::now();
     // Create a path to the desired file
     let f = File::open("de-en.txt").unwrap();
     let file = BufReader::new(&f);
-    
-    let re = Regex::new(&word).unwrap();
+
+    let re = Regex::new(&word_re).unwrap();
 
     for line in file.lines() {
-        
-        let l = match line {
-           Err(_) => "Problem".to_string(),
-           Ok(l) => l
-        }; 
 
+        let l = match line {
+            Err(_) => continue,
+            Ok(l) => l,
+        };
 
         if re.is_match(&l) {
-        println!("Line: {}", &l) 
-        }    }
-    println!("Finished Running"); 
+            print_match(&l);
+            break;
+        }
+    }
+    println!("Finished Running");
     let end = PreciseTime::now();
 
     println!("It took {} seconds.", start.to(end));
 
+}
+
+fn print_match(line: &str) {
+  //  let result = String::new();
+    let ger_eng: Vec<&str> = line.split("::").collect();
+    let eng = ger_eng[1].split(" | ");
+    let ger = ger_eng[0].split(" | ");
+    let mut pairs = ger.zip(eng);
+    while let Some((g, e)) = pairs.next() {
+        println!("{} – {}", g, e);
+       
+    }
+  
 }
 
 // Prints each argument on a separate line
